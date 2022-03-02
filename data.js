025d741d8eae34b23045913fc4f22a37ed1ef514
Bruce Dong <bruce.dong@societyone.com.au>
2022-03-02 11:21:12 +1100

    fix the bugs especially for the mode age

--- data.js
+++ data.js
@@ -1,4 +1,34 @@
 const data = [{
+//   "id": "a",
+//   "firstName": "Test",
+//   "lastName": "Name",
+//   "dob": "01-01-1991"
+// },{
+  "id": "b",
+  "firstName": "Test",
+  "lastName": "Name",
+  "dob": "01-01-1969"
+},{
+  "id": "c",
+  "firstName": "Test",
+  "lastName": "Name",
+  "dob": "01-01-1982"
+},{
+  "id": "c",
+  "firstName": "Test",
+  "lastName": "Name",
+  "dob": "01-01-1973"
+},{
+  "id": "d",
+  "firstName": "Test",
+  "lastName": "Name",
+  "dob": "01-01-1973"
+},{
+  "id": "e",
+  "firstName": "Test",
+  "lastName": "Name",
+  "dob": "01-01-1982"
+},{
   "id": "a",
   "firstName": "Test",
   "lastName": "Name",
@@ -9,31 +39,13 @@ const data = [{
   "lastName": "Name",
   "dob": "01-01-1962"
 },{
-  "id": "c",
-  "firstName": "Test",
-  "lastName": "Name",
-  "dob": "01-01-1973"
-},{
-  "id": "c",
-  "firstName": "Test",
-  "lastName": "Name",
-  "dob": "01-01-1973"
-},{
-  "id": "d",
-  "firstName": "Test",
-  "lastName": "Name",
-  "dob": "01-01-1994"
-},{
-  "id": "e",
-  "firstName": "Test",
-  "lastName": "Name",
-  "dob": "01-01-1982"
-},{
   "id": "f",
   "firstName": "Test",
   "lastName": "Name",
-  "dob": "01-01-1982"
+  "dob": "01-01-1980"
 }]
+// const data = []
+
 //Function: collect the dob from data
 const getDobArray = ( array ) => {
   const newArray = []
@@ -45,6 +57,12 @@ const getDobArray = ( array ) => {
 
 //Function: convert to age array
 const getAgeArray = ( today, array ) => {
+
+  if (!array.length) {
+    console.log("Sorry, the Array is empty.")
+    return []
+  }
+
   const newArray = []
   array.forEach((dob) => {
     newArray.push(((today - new Date(dob).getTime())/(1000*3600*24*365)).toFixed(2)) // get the time stamp to calculate the age
@@ -52,8 +70,25 @@ const getAgeArray = ( today, array ) => {
   return newArray
 }
 
+
+//Function: get the average age
+const getAverageAge = (array) => {
+  if (!array.length) {
+    console.log("Sorry, the Array is empty. There is no average age")
+    return "N/A"
+  }
+ 
+  const newArray = (array.reduce((a, b) => a + b)/ageArray.length).toFixed(2)
+  return newArray
+}
+
 //Function: get the median age
 const getMedianAge = (array) => {
+  if (!array.length) {
+    console.log("Sorry, the Array is empty. There is no median age.")
+    return "N/A"
+  }
+
   const newArray = [...array].sort()//for not changing the original array
   if(newArray.length % 2 === 0) {
     return (newArray[newArray.length/2]+newArray[newArray.length/2 - 1])/2 //if the length is even, then use the average of middle two numbers
@@ -65,21 +100,42 @@ const getMedianAge = (array) => {
 
 //Function: get the mode age
 const getModeAge = (array) => {
-  let age
-  let count = 0
-  for (let i = 0; i < array.length; i++) {
-    if (count === 0) {
-      age = array[i]
-      count++
-    }else{
-      if (age !== array[i]) {
-        count--
-      }else{
-        count++
+  let ageObj = {}
+  let modeAge = 0
+  let modeAgeNum = 0
+  if (!array.length) {
+    console.log("Sorry, the Array is empty. There is no mode age.")
+    return modeAge = "N/A"
+  }
+
+  array.forEach(item => {
+    ageObj[item] ? ageObj[item] += 1 : ageObj[item] = 1
+  })
+  if (Object.keys(ageObj).length === array.length) {
+    console.log("Sorry, the ages are so even that there is no mode age.")
+    return modeAge = "N/A"
+  } else {
+    console.log(ageObj);
+    for(let i in ageObj) {
+
+      // to find the mode age
+      if (ageObj[i] > modeAgeNum) {
+        modeAgeNum = ageObj[i]
+        modeAge = i
       }
     }
+
+    // If the mode age is not one
+    const ageObjValueArray = Object.values(ageObj)
+    const maxAgeNumInAgeObj = Math.max(...ageObjValueArray)
+    const numOfModeAge = ageObjValueArray.filter((item) => {
+      return item === maxAgeNumInAgeObj
+    })
+    if (numOfModeAge.length > 1) {
+      console.log("The mode age is more than one, the first mode age is: " + modeAge); 
+    }
+    return modeAge
   }
-  return age
 }
 
 const now = new Date().getTime() //get the right now time stamp
@@ -87,13 +143,15 @@ const dobArray = getDobArray(data)
 const ageArray = getAgeArray(now, dobArray).map(Number)
 
 //Result: the average age
-const averageAge = (ageArray.reduce((a, b) => a + b)/ageArray.length).toFixed(2)
+const averageAge = getAverageAge(ageArray)
+
 //Result: the median age
 const medianAge = getMedianAge(ageArray)
+
 //Result: the mode age
 const modeAge = getModeAge(ageArray)
 
-console.log(ageArray);
+//Result: json obj
 const result = JSON.stringify({
   average: averageAge.toString(),
   median: medianAge.toString(),
